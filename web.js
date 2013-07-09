c555841ac22600f09336261a4e83678bc5a65fd3
neerajzap <neerajzap@gmail.com>
2013-07-10 01:59:16 +0530

    chaged web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+var buffer = new Buffer(25);
+var outfile = "index.html";
+buffer.write(fs.readFileSync(outfile,"utf-8"),"utf-8");
+
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
