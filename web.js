4a7f45dd7736a3015f5643ed9db0e13ce2d6ad8c
neerajzap <neerajzap@gmail.com>
2013-07-13 01:38:15 +0530

    web.js bug fixed

--- web.js
+++ web.js
@@ -5,12 +5,11 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
 var outfile = "index.html";
-var buffer = new Buffer(fs.readFileSync(outfile,"utf-8"),40);
-
+var buffer = new Buffer(fs.readFileSync(outfile,"utf-8"));
   response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+ });
